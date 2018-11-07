7cfcd659a70de7cb002decddef166df38a66869e
Maciej Kliś <maciej.klis@thecoders.pl>
2018-11-07 16:02:19 +0100

    FEAT: Possibility to record sounds on 4 different channels

--- README.md
+++ README.md
@@ -5,6 +5,6 @@
 ### TODO:
 1. ~~Download example sounds~~
 2. ~~Bind sounds to keys~~
-3. Possibility to record sounds, on four different channels
+3. ~~Possibility to record sounds, on four different channels~~
 4. Possibility to play one or more channels at one time
 5. Make friendly look :)
--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,51 @@
 "use strict";
 
-// //array of bind keys
-// let bindKeys = ["q", "w", "e", "r", "a", "s", "d", "f"];
+let record0 = [];
+let record1 = [];
+let record2 = [];
+let record3 = [];
+
+function addSoundToRecord(event){
+	let recordStatus = document.getElementsByName('recordSwitch')[1].checked;
+	let recordTape = parseInt(document.querySelector('input[name="recordChannel"]:checked').value);
+
+	if(recordStatus === true){
+		switch (recordTape) {
+			case 0 :
+				if(record0.length < 10){
+					record0.push(event.key);
+					addLetterToHTML(record0, document.querySelector("#firstRecord"));
+				}
+				break;
+			case 1 :
+				if(record1.length < 10){
+					record1.push(event.key);
+					addLetterToHTML(record1, document.querySelector("#secondRecord"));
+				}
+				break;
+			case 2 :
+				if(record2.length < 10){
+					record2.push(event.key);
+					addLetterToHTML(record2, document.querySelector("#thridRecord"));
+				}
+				break;
+			case 3 :
+				if(record3.length < 10){
+					record3.push(event.key);
+					addLetterToHTML(record3, document.querySelector("#fourthRecord"));
+				}
+				break;
+		}
+	}
+}
+
+function addLetterToHTML(array, tapeSelector){
+	tapeSelector.innerHTML = array.join().toUpperCase();
+}
 
 //bind keys to sounds
 document.body.addEventListener('keypress' , (event) =>{
+		addSoundToRecord(event);
 		switch(event.key){
 		case 'q' :
 			q();
@@ -31,45 +72,45 @@ document.body.addEventListener('keypress' , (event) =>{
 			f();
 			break;
 	}
-
-	function q(){
-		let q = document.querySelector("#q");
-		q.currentTime = 0;
-		q.play();
-	}
-	function w(){
-		let w = document.querySelector("#w");
-		w.currentTime = 0;
-		w.play();
-	}
-	function e(){
-		let e = document.querySelector("#e");
-		e.currentTime = 0;
-		e.play();
-	}
-	function r(){
-		let r = document.querySelector("#r");
-		r.currentTime = 0;
-		r.play();
-	}
-	function a(){
-		let a = document.querySelector("#a");
-		a.currentTime = 0;
-		a.play();
-	}
-	function s(){
-		let s = document.querySelector("#s");
-		s.currentTime = 0;
-		s.play();
-	}
-	function d(){
-		let d = document.querySelector("#d");
-		d.currentTime = 0;
-		d.play();
-	}
-	function f(){
-		let f = document.querySelector("#f");
-		f.currentTime = 0;
-		f.play();
-	}
 })
+
+function q(){
+	let q = document.querySelector("#q");
+	q.currentTime = 0;
+	q.play();
+}
+function w(){
+	let w = document.querySelector("#w");
+	w.currentTime = 0;
+	w.play();
+}
+function e(){
+	let e = document.querySelector("#e");
+	e.currentTime = 0;
+	e.play();
+}
+function r(){
+	let r = document.querySelector("#r");
+	r.currentTime = 0;
+	r.play();
+}
+function a(){
+	let a = document.querySelector("#a");
+	a.currentTime = 0;
+	a.play();
+}
+function s(){
+	let s = document.querySelector("#s");
+	s.currentTime = 0;
+	s.play();
+}
+function d(){
+	let d = document.querySelector("#d");
+	d.currentTime = 0;
+	d.play();
+}
+function f(){
+	let f = document.querySelector("#f");
+	f.currentTime = 0;
+	f.play();
+}
